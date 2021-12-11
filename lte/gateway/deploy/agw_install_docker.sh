4a6e4f4fe5f38a84f9ab179a0563a1532620e07f
quentinDERORY <15911421+quentinDERORY@users.noreply.github.com>
2021-12-10 17:18:19 -0800

    feat(ci): GHA workflow to generate docker AWS AMI for AGW
    Draft of GHA workflow to launch AGW load test
    Signed-off-by: quentinDERORY <15911421+quentinDERORY@users.noreply.github.com>

--- lte/gateway/deploy/agw_install_docker.sh
+++ lte/gateway/deploy/agw_install_docker.sh
@@ -10,6 +10,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO: Actually install docker and add MAGMA_USER to docker group
+
 MODE=$1
 WHOAMI=$(whoami)
 MAGMA_USER="ubuntu"
