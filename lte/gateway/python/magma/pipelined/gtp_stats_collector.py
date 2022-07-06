16702f1943e30314fda256ba64162eeb7b0f3c14
Paulo Henrique Calaes Oliveira <paulo@radtonics.com>
2022-07-06 14:31:10 -0300

    fix(agw): change regexp to accept flow and ip address
    Signed-off-by: Paulo Henrique Calaes Oliveira <paulo@radtonics.com>

--- lte/gateway/python/magma/pipelined/gtp_stats_collector.py
+++ lte/gateway/python/magma/pipelined/gtp_stats_collector.py
@@ -42,10 +42,10 @@ class OVSDBCommandResult(NamedTuple):
 
 
 interface_group = r"(?P<Interface>\w+)"
-remote_ip_group = r"(?P<remote_ip>.*)"
+remote_ip_group = r"[\"]?(?P<remote_ip>[^}\"]*)"
 rx_bytes_group = r"(?P<rx_bytes>\d+)"
 tx_bytes_group = r"(?P<tx_bytes>\d+)"
-stats_re_str = r'{}(.*)(remote_ip="{}")(.*)?rx_bytes={}.+tx_bytes={}'
+stats_re_str = r'{}(.*)(remote_ip={})(.*)?rx_bytes={}.+tx_bytes={}'
 
 interface_tx_rx_stats_re = re.compile(
     stats_re_str.format(
